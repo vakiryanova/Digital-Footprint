dd37d04f230012f463b0d3b7f4e68e9731e383af
vakiryanova <47382680+vakiryanova@users.noreply.github.com>
2019-11-26 17:52:24 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -4,26 +4,22 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import logging
-import logs
 
+#область видимости: доступ к каким данным пользователя запрашивает приложение
 SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly', #список курсов
-          'https://www.googleapis.com/auth/classroom.rosters.readonly', #список студентов
-          'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly', 
-          'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
-          'https://www.googleapis.com/auth/classroom.profile.emails',
-          'https://www.googleapis.com/auth/classroom.profile.photos'
+          'https://www.googleapis.com/auth/classroom.rosters.readonly', #список студентов и преподавателей
+          'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly', #работы студентов
+          'https://www.googleapis.com/auth/classroom.profile.emails', #электронная почта пользователей
+          'https://www.googleapis.com/auth/classroom.profile.photos' #аватары пользователей
          ]
 
 def auth():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
     if os.path.exists('auth/token.pickle'):
         with open('auth/token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+    #если токенов нет, откроется страница авторизации Google
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -31,10 +27,11 @@ def auth():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'auth/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+        #токены сохранены для последующего использования
         with open('auth/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('classroom', 'v1', credentials=creds)
-    logger.log(logging.INFO, 'аутентификация прошла успешно')
     return service
+
+auth()
